a2713fcdcbbb9b69790e577e5738a0435f48d64b
Hiroyuki Suzuki <suzuki.wotsuku.hiroyuki.do.work@gmail.com>
2025-04-26 14:57:02 +0900

    add just repeat prompt

---
+++ src/dullahan/llms/Functional_RepeatPrompt.py
@@ -0,0 +1,13 @@
+from ..defs.datadef import ChatMessageData
+
+class Functional_Echo:
+    @classmethod
+    def call(cls, messages: list[ChatMessageData]) -> str:
+        last_message = ""
+        for msg in reversed(messages):
+            if msg.role == "assistant":
+                last_message = msg.message
+                break
+        return last_message
+
+
